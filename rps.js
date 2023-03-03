b0db041702144315764ed93032230a71a20a7b4f
Zando411 <123271994+Zando411@users.noreply.github.com>
2023-03-02 23:15:23 -0800

    add game logic and round looping

--- rps.js
+++ rps.js
@@ -1,6 +1,7 @@
 function getComputerChoice() {
   const choices = ["Rock", "Paper", "Scissors"];
   const randomIndex = Math.floor(Math.random() * 3);
+
   return choices[randomIndex];
 }
 
@@ -12,4 +13,32 @@ function getPlayerChoice() {
 const playerChoice = getPlayerChoice();
 const computerChoice = getComputerChoice();
 
-console.log(playerChoice, computerChoice);
+let message = "";
+
+function round(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) {
+    message = "It's a tie!";
+  } else if (
+    (playerChoice === "Rock" && computerChoice === "Scissors") ||
+    (playerChoice === "Paper" && computerChoice === "Rock") ||
+    (playerChoice === "Scissors" && computerChoice === "Paper")
+  ) {
+    message = "You win!";
+  } else {
+    message = "You lose!";
+  }
+}
+
+function game() {
+  const computerChoice = getComputerChoice();
+  const playerChoice = getPlayerChoice();
+
+  round(playerChoice, computerChoice);
+  console.log(
+    `You chose ${playerChoice}. Computer chose ${computerChoice}. ${message}`
+  );
+}
+
+for (let i = 0; i < 5; i++) {
+  game();
+}
